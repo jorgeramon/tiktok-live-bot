0402c0fa997c72d13971d8aaa13a57ada466a85b
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-05-13 01:53:35 -0600

    Fix?

--- src/core/command-resolver.ts
+++ src/core/command-resolver.ts
@@ -47,7 +47,8 @@ export class CommandResolver {
                 user_nickname: message.user_nickname,
                 user_picture: message.user_picture,
                 stream_id: message.stream_id,
-                argument: normalized_comment
-            }
+                argument: normalized_comment.slice(0, command.length)
+            });
+        }
     }
-    }
+}
