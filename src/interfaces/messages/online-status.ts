7bcc28292c80b3d3aae6a369c6a144d6a130b084
Jorge Ramon <jorge.ramon2128@gmail.com>
2025-05-29 16:35:39 -0600

    RoomInfo is optional in interface

--- src/interfaces/messages/online-status.ts
+++ src/interfaces/messages/online-status.ts
@@ -3,5 +3,5 @@ import { IOnlineMessage } from '@/interfaces/messages/online';
 export interface IOnlineStatusMessage {
   owner_username: string;
   is_online: boolean;
-  room_info: IOnlineMessage;
+  room_info?: IOnlineMessage;
 }
